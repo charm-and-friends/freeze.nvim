65d0abdaa280bb2761a4e968cc078f273c27ae7f
isabel <isabel@isabelroses.com>
2024-03-27 19:24:10 +0000

    feat: allow custom config

---
+++ lua/charm-freeze/init.lua
@@ -0,0 +1,138 @@
+local M = {}
+
+-- We require command to give, this also allows us to have a custom command
+M.required_options = {
+	command = "freeze",
+}
+
+-- We allow the user to provide custom options, if none are provided the command will use its default args
+M.allowed_opts = {
+	command = "string",
+	output = { "string", "function" },
+	window = "boolean",
+	padding = { "table", "string" },
+	margin = { "table", "string" },
+	background = "string",
+	theme = "string",
+	line_numbers = "boolean",
+	line_height = "number",
+	language = "string",
+	font = {
+		size = "number",
+		family = "string",
+		ligatures = "boolean",
+	},
+	shadow = {
+		blur = "number",
+		x = "number",
+		y = "number",
+	},
+	border = {
+		radius = "number",
+		width = "number",
+		color = "string",
+	},
+}
+
+M.parse_options = function(opts)
+	local options
+
+	-- Ensure the input is a table
+	vim.validate({
+		opts = { opts, "table" },
+	})
+
+	-- Ensure that the user has provided the required options
+	options = vim.tbl_deep_extend("force", M.required_options, opts or {})
+
+	-- Ensure that the user has not provided any options that are not allowed
+	if M.allowed_opts then
+		for k, v in pairs(opts) do
+			vim.validate({
+				[k] = { v, M.allowed_opts[k] },
+			})
+		end
+	end
+
+	return options
+end
+
+-- Generate the command line arguments
+M.get_arguments = function(args, options)
+	local cmd = {}
+	local value = nil
+	table.insert(cmd, options.command)
+	for k, v in pairs(options) do
+		if k ~= "command" then
+			table.insert(cmd, "--" .. string.gsub(k, "_", "-"))
+			if type(v) == "function" then
+				value = v(args)
+			else
+				value = v
+			end
+			table.insert(cmd, value)
+		end
+	end
+	return cmd
+end
+
+M.format_lines = function(cmdline, args)
+	local begin_line = args.line1 - 1
+	local finish_line = args.line2
+
+	if args.range == 0 then
+		begin_line = 0
+		finish_line = -1
+	end
+
+	local marks = vim.api.nvim_buf_get_mark(vim.api.nvim_win_get_buf(0), "h")[1]
+	if marks > 0 then
+		local hl
+		if args.range == 0 or (args.line1 and marks >= begin_line and marks <= finish_line) then
+			hl = marks - begin_line
+			table.insert(cmdline, "--lines ")
+			table.insert(cmdline, hl)
+		end
+	end
+
+	local lines = vim.api.nvim_buf_get_lines(vim.api.nvim_win_get_buf(0), begin_line, finish_line, false)
+
+	return lines, cmdline
+end
+
+M.start = function(args, options)
+	local lines = nil
+
+	-- start building the base of the command from the options
+	local base_cmdline = M.get_arguments(args, options)
+	-- parse buffer into lines, based on arguments from neovim, reshapes cmdline
+	lines, base_cmdline = M.format_lines(base_cmdline, args)
+
+	-- try to get the language from neovim's buffer filetype
+	local cmd = vim.tbl_extend("error", base_cmdline, {})
+	table.insert(cmd, "--language")
+	table.insert(cmd, vim.bo.filetype)
+
+	-- run the command and get the output
+	local ret = vim.fn.system(cmd, lines)
+	if string.find(ret, "WROTE") then
+		return vim.notify("File saved to" .. string.sub(ret, 8), vim.log.levels.INFO, { title = "charm-freeze.nvim" })
+	else
+		return vim.notify("freeze returned: " .. ret, vim.log.levels.WARN, { title = "charm-freeze.nvim" })
+	end
+end
+
+-- define commands for neovim
+M.setup = function(opts)
+	local options = M.parse_options(opts)
+
+	vim.api.nvim_create_user_command("Freeze", function(args)
+		M.start(args, options)
+	end, {
+		desc = "convert range to code image representation",
+		force = false,
+		range = true,
+	})
+end
+
+return M
