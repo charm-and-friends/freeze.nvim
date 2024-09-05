3746d9b963396aa609e409c8e81a7d1d273d80dd
isabel <isabel@isabelroses.com>
2024-09-05 20:33:09 +0100

    ci: add more forms of checks

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            selene
+            stylua
+          ];
+        };
+      });
+    };
+}
